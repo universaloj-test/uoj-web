588293937ed47f6daad104400b4ae85a81fc8f66
billchenchina <billchenchina2001@gmail.com>
2018-01-11 10:05:13 +0800

    update

--- app/upgrade/UOJ-Edited-1.0.1/down.sql
+++
@@ -1,4 +0,0 @@
-use app_uoj233;
-alter table user_info drop column school;
-alter table user_info drop column grade;
-alter table user_info drop column real_name;
--- app/upgrade/UOJ-Edited-1.0.1/up.sql
+++
@@ -1,4 +0,0 @@
-use app_uoj233;
-alter table user_info add column school varchar(200);
-alter table user_info add column grade varchar(100);
-alter table user_info add column real_name varchar(100);
